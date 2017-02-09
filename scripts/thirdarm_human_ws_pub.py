ebd0096a06832dc55e4bdc1ea73f080c256a83a0
vighv <vighnesh.vatsal@gmail.com>
2017-02-09 03:25:18 -0500

    v2 and human point cloud

--- scripts/thirdarm_human_ws_pub.py
+++ scripts/thirdarm_human_ws_pub.py
@@ -7,38 +7,47 @@ import arbotix_msgs.srv
 from std_msgs.msg import String, Float64
 from geometry_msgs.msg import Vector3, Twist
 from sensor_msgs.msg import JointState
-i = 0
+
+j = 0
+
 def callback(data):
 	pub = rospy.Publisher('/third_arm_joints', JointState, queue_size=1)
 	pub_human = rospy.Publisher('/joint_states', JointState, queue_size=1)
-	global i
-	i += 1
-	state = data
-	val1 = -1.57 + 3.14*random.betavariate(1, 1)
-	val2 = -1.57 + (30*math.pi/180 + 1.57)*random.betavariate(0.6, 0.6)
-	val3 = 0.15*random.betavariate(0.4, 0.4)
-	if i<100:
-		state.position = [val1,val2,val3,val1,val1,val1]
 	
-	# if i < 1.57 and incr == 1:
-	# 	i += 0.01
-	# elif incr == -1:
-	# 	i -= 0.01
+	global j
+	j += 1
+	if j<math.pow(10,9):
+		state = data
+		val1 = -3.14 + 6.28*random.betavariate(1, 1)
+		val2 = -3.14 + (30*math.pi/180 + 3.14)*random.betavariate(0.6, 0.6)
+		val3 = 0.15*random.betavariate(0.4, 0.4)
+		#if i<100:
+		state.position = [val1,val2,val3,val1,val1,val1]
+		
+		h_len = len(data.name)
+		h_state = data
+		h_state.position = [0.0 for i in range(h_len)]
+		h_state.position[0:5] = [val1,val2,val3,val1,val1,val1]#state.position
 
-	# if i>1.57:
-	# 	incr = -1
-	# elif i<0:
-	# 	incr = 1
-	h_len = len(data.position)
-	h_state = data
+		hval1 = -1.57 + 3.14*random.betavariate(0.6, 0.6)
+		hval2 = -1.57 + 3.927*random.betavariate(0.55, 0.55)
+		hval3 = -1.57 + 3.14*random.betavariate(0.6, 0.6)
+		hval4 = 1.57*random.betavariate(0.5, 0.5)
+		h_state.position[12:15] = [hval1,hval2,hval3,hval4]
 
-	pub.publish(state)
+		pub.publish(state)
+		pub_human.publish(h_state)
+	
+	
 
 if __name__ == '__main__':
 	rospy.init_node('publish_states_thirdarm_rviz')
 	#rate = rospy.Rate(5)
 	#while not rospy.is_shutdown():
+	
+	
 	rospy.Subscriber('/joint_states', JointState, callback)
+		
 	rospy.spin()
 	#rate.sleep()
 	
